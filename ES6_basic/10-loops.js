11f025a69161f0a1e34997fb075d0278f9f11029
Christian <christianda3@gmail.com>
2024-07-16 22:06:49 -0400

    Refactor loop in appendToEachArrayValue function to use for...in loop with hasOwnProperty check

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
-/* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx in array) {
-     let value = array[idx];
-    array[idx] = appendString + value;
-  }
+    for (const key in array) {
+      if (Object.prototype.hasOwnProperty.call(array, key)) {
+        array[key] = appendString + array[key];
+      }
+    }
 
-  return array;
-}
+    return array;
+  }
