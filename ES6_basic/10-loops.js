42e65ad682f833336e8fdbf8e301631ab1585a62
Christian <christianda3@gmail.com>
2024-07-16 22:00:19 -0400

    feat: Add function to append a string to each value in an array

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+  for (const key in array) {
+    if (Object.prototype.hasOwnProperty.call(array, key)) {
+      array[key] = appendString + array[key];
+    }
+  }
+
+  return array;
+}
