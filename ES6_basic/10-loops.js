5840c66914a5b9b57a9c20231cfbd18965272be2
Christian <christianda3@gmail.com>
2024-07-16 22:02:02 -0400

    Refactor loop in appendToEachArrayValue function to use index variable instead of key

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
-  for (const key in array) {
-    if (Object.prototype.hasOwnProperty.call(array, key)) {
-      array[key] = appendString + array[key];
-    }
+  for (const idx in array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
   }
 
   return array;
