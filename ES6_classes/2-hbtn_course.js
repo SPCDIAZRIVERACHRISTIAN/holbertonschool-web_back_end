5c4799e34a8fd1b95b9c9824bca6be68ea4286d5
Christian <christianda3@gmail.com>
2024-07-19 14:00:37 -0400

    Refactor HolbertonCourse constructor to validate input types

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,23 +1,24 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+        throw new TypeError('name must be a string');
+      }
+      if (typeof length !== 'number') {
+        throw new TypeError('length must be an integer');
+      }
+      if (!Array.isArray(students)) {
+        throw new TypeError('students is not an array');
+      }
+      if (!students.every(item => typeof item == 'string')) {
+        throw new TypeError('Expected items to be string');
+      }
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
   SetCourse(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('length must be an integer');
-    }
-    if (!Array.isArray(students)) {
-      throw new TypeError('students is not an array');
-    }
-    if (!String.every((item) => typeof item === 'string')) {
-      throw new TypeError('Expected items to be string');
-    }
+
     this._name = name;
     this._length = length;
     this._students = students;
