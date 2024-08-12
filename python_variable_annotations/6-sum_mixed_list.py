108df1aa62f4f1a81b703696a3c6233f0acb60c6
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-08-12 18:27:44 -0400

    feat: Update sum_mixed_list function to accept a list of mixed integers and floats

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,9 +2,9 @@
 
 '''Takes a list of integers and floats and returns their sum as float'''
 
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_list: Union[int, float]) -> float:
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     '''Returns a sum of list in floats'''
     return sum(mxd_list)
