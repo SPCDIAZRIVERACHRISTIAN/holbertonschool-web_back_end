57d2f4d96344f931878212bc3b1005dff0f4d25c
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-08-12 18:13:33 -0400

    Added task 5 of the proyect

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+'''a function that returns a sum of listed floats'''
+
+
+def sum_list(input_list: List[float]) -> float:
+    return sum(input_list)
