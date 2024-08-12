2652ea054109a63368370c1ea11599887f4aa4c0
SPCDIAZRIVERACHRISTIAN <christianda3@gmail.com>
2024-08-12 18:18:08 -0400

    docs: Add type annotations and documentation for sum_list function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -6,5 +6,13 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    '''Adds all the contents of the list provided'''
+    """
+    Calculate the sum of all elements in the given list.
+
+    Args:
+        input_list (List[float]): A list of floating-point numbers.
+
+    Returns:
+        float: The sum of all elements in the list.
+    """
     return sum(input_list)
