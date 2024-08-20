994ff020c52b9da1633d79f693bd19525c63f39f
Christian <christianda3@gmail.com>
2024-08-20 08:22:59 -0400

    refactor async_generator to use "_" as a loop variable

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,6 +10,6 @@ import typing
 
 async def async_generator() -> typing.AsyncGenerator[float, None]:
     '''this loop iterates 10 times and yields a random float and waits 1 second'''
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
