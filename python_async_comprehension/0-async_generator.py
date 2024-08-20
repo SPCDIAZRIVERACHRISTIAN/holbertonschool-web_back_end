6796cbaf649492a2f892ce4c86263515b4ac538c
Christian <christianda3@gmail.com>
2024-08-20 08:18:38 -0400

    Refactor task_wait_n function to include type annotations and sort delays

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''This function loops 10 times asynchronously wait 1 second and yield
+a random number'''
+
+
+import random
+import asyncio
+import typing
+
+
+async def async_generator() -> typing.AsyncGenerator[float, None]:
+    '''this loop iterates 10 times and yields a random float and waits 1 second'''
+    for i in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -14,3 +14,6 @@ def measure_time(n: int, max_delay: int) -> float:
     end_time = time.perf_counter()
     total_time = end_time - start_time
     return total_time / n
+pass code:
+
+ 
