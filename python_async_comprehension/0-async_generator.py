7b8b125f08e4299a042d9923481f58d9b17c87b1
Christian <christianda3@gmail.com>
2024-08-20 08:25:36 -0400

    Refactor async_generator to yield random floats before waiting

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,7 +9,8 @@ import typing
 
 
 async def async_generator() -> typing.Generator[float, None, None]:
-    '''this loop iterates 10 times and yields a random float and waits 1 second'''
+    '''this loop iterates 10 times and yields
+    a random float and waits 1 second'''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
