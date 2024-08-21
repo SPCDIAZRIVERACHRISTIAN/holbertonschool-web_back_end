16772c3fbcd75020067f535c830fc03b28e60dbd
Christian <christianda3@gmail.com>
2024-08-21 16:01:55 -0400

    Refactor get_page method to accept page and page_size parameters

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -48,21 +48,31 @@ class Server:
         end_index = start_index + page_size
         return (start_index, end_index)
 
-    def get_hyper(self, page: int = 1, page_size:
-                  int = 10) -> typing.Dict:
-        '''Returns a dictionary'''
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        start_index, end_index = self.index_range(page, page_size)
-        dataset = self.dataset()
-        data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(dataset) / page_size)
+    def get_hyper(self, page: int = 1, page_size: int = 10):
+        """Returns a dictionary of hypermedia key-value pairs"""
 
-        return {
+        start_index, end_index = self.index_range(page, page_size)
+
+        prev_page = None
+        if (page > 1):
+            prev_page = page - 1
+
+        next_page = None
+        if (len(self.dataset()) > end_index):
+            next_page = page + 1
+
+        total_pages = int(len(self.dataset()) / 10)
+        if (page_size > 0):
+            total_pages = int(len(self.dataset()) / page_size)
+
+        hyper_dict = {
+            "page_size": len(self.get_page(page, page_size)),
             "page": page,
-            "page_size": page_size,
-            "data": data,
-            "next_page": page + 1 if end_index < len(dataset) else None,
-            "prev_page": page - 1 if page > 1 else None,
+            "data": self.get_page(page, page_size),
+            "next_page": next_page,
+            "prev_page": prev_page,
             "total_pages": total_pages
         }
+
+        return hyper_dict
+
