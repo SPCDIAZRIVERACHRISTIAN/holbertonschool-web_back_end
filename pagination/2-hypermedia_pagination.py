61f978d7cc8f7716e3697cd9de4b175e20707a0d
Christian <christianda3@gmail.com>
2024-08-21 14:09:58 -0400

    Refactor get_page method to remove unnecessary assertions

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -27,7 +27,8 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> typing.List[typing.List]:
+    def get_page(self, page: int = 1, page_size:
+                 int = 10) -> typing.List[typing.List]:
         """this function gets the page"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
@@ -48,7 +49,7 @@ class Server:
         return (start_index, end_index)
 
     def get_hyper(self, page: int = 1, page_size:
-                 int = 10) -> typing.Dict:
+                  int = 10) -> typing.Dict:
         '''Returns a dictionary'''
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
