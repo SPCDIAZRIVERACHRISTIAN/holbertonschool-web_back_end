b89a2b020ced0ac9c324cede457173456b71ea7d
Christian <christianda3@gmail.com>
2024-08-21 09:27:34 -0400

    Refactor pagination helper function to improve readability

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,7 +6,8 @@ import typing
 
 
 def index_range(page, page_size) -> typing.Tuple:
-    ''' Returns a tuple of size two containing a start index and an end index. '''
+    ''' Returns a tuple of size two containing
+    a start index and an end index. '''
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
