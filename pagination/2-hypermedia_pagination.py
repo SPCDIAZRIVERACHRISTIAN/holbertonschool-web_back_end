cec702e0074a78f12075f35044895044e6747b05
Christian <christianda3@gmail.com>
2024-08-21 14:06:58 -0400

    Refactor get_page method to remove unnecessary assertions

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -5,7 +5,7 @@
 
 import csv
 import math
-from typing import List, Tuple
+import typing
 
 
 class Server:
@@ -16,7 +16,7 @@ class Server:
     def __init__(self):
         self.__dataset = None
 
-    def dataset(self) -> List[List]:
+    def dataset(self) -> typing.List[typing.List]:
         """Cached dataset
         """
         if self.__dataset is None:
@@ -27,7 +27,7 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> typing.List[typing.List]:
         """this function gets the page"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
@@ -41,7 +41,7 @@ class Server:
         return data[start_index:end_index]
 
     @staticmethod
-    def index_range(page, page_size) -> Tuple[int, int]:
+    def index_range(page, page_size) -> typing.Tuple[int, int]:
         """this function returns a tuple with the start and end indexes"""
         start_index = (page - 1) * page_size
         end_index = start_index + page_size
@@ -49,4 +49,19 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size:
                  int = 10) -> typing.Dict:
+        '''Returns a dictionary'''
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start_index, end_index = self.index_range(page, page_size)
+        dataset = self.dataset()
+        data = self.get_page()
+        total_pages = math.ceil(len(dataset) / page_size)
 
+        return {
+            "page": page,
+            "page_size": page_size,
+            "data": data,
+            "next_page": page + 1 if end_index < len(dataset) else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
