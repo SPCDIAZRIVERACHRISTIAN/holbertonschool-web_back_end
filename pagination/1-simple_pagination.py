3345bbee498ee32fc987446450ab40643dc81ad1
Christian <christianda3@gmail.com>
2024-08-21 10:16:07 -0400

    feat: Improve pagination performance and readability

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-''''''
+'''Server class for pagination'''
 
 import csv
 import math
@@ -26,10 +26,13 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> typing.List[typing.List]:
+    def get_page(self, page: int = 1, page_size:
+                 int = 10) -> typing.List[typing.List]:
         """Returns a page of the dataset"""
-        assert isinstance(page, int) and page > 0, "Page must be an integer greater than 0"
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be an integer greater than 0"
+        assert isinstance(page, int) and page > 0, \
+            "Page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "Page size must be an integer greater than 0"
 
         start_index, end_index = self.index_range(page, page_size)
         dataset = self.dataset()
@@ -41,7 +44,8 @@ class Server:
 
     @staticmethod
     def index_range(page: int, page_size: int) -> typing.Tuple[int, int]:
-        """Returns a tuple of size two containing a start index and an end index"""
+        """Returns a tuple of size two containing
+        a start index and an end index"""
         start_index = (page - 1) * page_size
         end_index = start_index + page_size
         return start_index, end_index
