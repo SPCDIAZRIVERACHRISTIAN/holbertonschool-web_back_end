32f52a80fea62189df81591bed601fee13d7dc1b
Christian <christianda3@gmail.com>
2024-08-22 11:45:31 -0400

    Refactor nginx_stats function to include docstring and improve code readability

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-''''''
+'''provides stats about nginx logs'''
 
 
 import pymongo
 
 
 def nginx_stats():
+    '''displays logs in the nginx'''
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client.logs
     collection = db.nginx
@@ -17,8 +18,10 @@ def nginx_stats():
     for method in methods:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    status_check = collection.count_documents
+    ({"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
+
 
 if __name__ == "__main__":
     nginx_stats()
