6182483907ca48d4fffa71d1dd2416415b3302e6
Christian <christianda3@gmail.com>
2024-09-17 11:13:57 -0400

    Refactor stdin.js to use readline module for user input and update console.log statement

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,9 +2,13 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-  console.log(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit();
 });
